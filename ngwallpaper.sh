96f3544e3ca6dac7cfffebf27b451527a66898e3
Saman Barghi <saman.b@gmail.com>
2012-08-29 16:19:33 -0400

    Added the script.

---
+++ ngwallpaper.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+# This Script downloads National Geographic Photo of the day, and sets it as desktop background (gnome, unity)
+# Copyright (C) 2012 Saman Barghi - All Rights Reserved
+# Permission to copy, modify, and distribute is granted under GPLv3
+# Last Revised 29 Augst 2012 
+
+
+#check for network
+while [ -z "`curl -s --head http://google.com/ | head -n 1 | grep 'HTTP/1.[01]'`" ]
+do
+	echo "Network is down!!"	
+	sleep 1800 
+done 
+#######################
+
+#Change directory to where the script resides. 
+BASEDIR=$(dirname $0)
+echo $BASEDIR
+cd $BASEDIR
+#######################
+
+#getting the image URL  
+img=`curl http://photography.nationalgeographic.com/photography/photo-of-the-day/ | grep download_link | awk -F\" '{print $4}'`
+
+#check to see if there is any wallpaper to download
+if [ -n "$img" ]
+then
+	img_file=`basename $img`
+	if [ -f "$img_file" ]
+	then
+		echo "File already exists"
+	else
+		wget $img  
+		#set the current image as wallpaper
+		gsettings set org.gnome.desktop.background picture-uri file:///`pwd`/$img_file
+		echo "Wallpaper was updated successfully"
+	fi
+else
+	echo "No Wallpaper today" 
+fi
