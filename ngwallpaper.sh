91e218924fb6c930d854703133fdd88c3b889e42
Saman Barghi <saman.b@gmail.com>
2016-09-17 15:58:12 -0400

    Updated the link and using md5 to save files
    Updated the link, and added md5 to save the file.
    
    Addressing issue #2.

--- ngwallpaper.sh
+++ ngwallpaper.sh
@@ -20,21 +20,24 @@ cd $BASEDIR
 
 #getting the image URL
 #img=`curl http://photography.nationalgeographic.com/photography/photo-of-the-day/ | grep download_link | awk -F\" '{print $4}'`
-img="http:$(curl http://photography.nationalgeographic.com/photography/photo-of-the-day/ | awk 'found && /<\/div>/ {exit}; found ;/class="primary_photo"/ {found=1}' | grep -oP '(?<=img src=")[^"]*(?=")')"
-
+#img="http:$(curl http://photography.nationalgeographic.com/photography/photo-of-the-day/ | awk 'found && /<\/div>/ {exit}; found ;/class="primary_photo"/ {found=1}' | grep -oP '(?<=img src=")[^"]*(?=")')"
+img="$(curl http://www.nationalgeographic.com/photography/photo-of-the-day/ -s | grep -oP '(?<='\''aemLeadImage'\'': '\'')[^'\'']*')"
 
 #check to see if there is any wallpaper to download
 if [ -n "$img" ]
 then
-	img_file=`basename $img`
+    img_base=`basename $img`
+    img_md5=`echo -n $img_base | md5sum | cut -f1 -d" "`
+	img_file="$img_md5.jpg"
+
 	if [ -f "$img_file" ]
 	then
 		echo "File already exists"
 	else
-		wget $img
+        curl "$img" > $img_file
 		#set the current image as wallpaper
 		#gsettings set org.gnome.desktop.background picture-uri file:///`pwd`/$img_file
-		echo "Wallpaper was updated successfully"
+		echo "Wallpaper downloaded successfully and saved as $img_file"
 	fi
 else
 	echo "No Wallpaper today"
